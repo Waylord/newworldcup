c66707f5753bd683f696488cd12affedd447d92c
Waylord <42625377+Waylord@users.noreply.github.com>
2019-05-22 16:43:09 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include "../include/functions.h"
+
 
 int main(){
-    
+    int group_matches = (groups * combin(teams / group));
+    score result_group_matches[group_matches];
 }
