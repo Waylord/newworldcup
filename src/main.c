e7d00cbde08b13309536c3c75e48d4c1cc30b882
Waylord <42625377+Waylord@users.noreply.github.com>
2019-05-22 22:56:54 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,4 +5,6 @@
 int main(){
     int group_matches = (groups * combin(teams / group));
     score result_group_matches[group_matches];
+    char team_name[teams + 1 /* TBD option */][4]; //FIFA Trigramme list
+    
 }
